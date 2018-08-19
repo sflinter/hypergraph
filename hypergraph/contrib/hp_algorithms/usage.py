7159c29ea5465a2bc7fded525cb6c76e5b9f29b9
nicola mariella <ejpiminus1eq0@gmail.com>
2018-08-19 14:31:08 +0100

    moved hp algos to branch hp_algorithms, we will merge with master when the dev will be finished.

--- hypergraph/contrib/hp_algorithms/usage.py
+++
@@ -1,7 +0,0 @@
-from techniques import RandomSearch
-
-objective = 'some_objective'
-search_space = 'some_space'
-
-rs = RandomSearch(objective, search_space=search_space, trials=0, random_seed=1)
-rs.run()
