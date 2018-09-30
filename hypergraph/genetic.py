a790ed19d09cdd6f8d67abf0e3af260b8dc72b3e
nicola mariella <ejpiminus1eq0@gmail.com>
2018-09-30 22:45:11 +0100

    tournament selection strategy

--- hypergraph/genetic.py
+++ hypergraph/genetic.py
@@ -1,7 +1,7 @@
 import numpy as np
 from . import graph as hgg
 from . import tweaks
-import numbers
+import itertools
 
 
 class GeneticBase:
@@ -132,25 +132,51 @@ class TournamentSelection:
         Perform a tournament selection on the provided population. The population ust be ordered by decreasing
         score (thus the best individual is the first).
         :param fitness_ordered_pop:
-        :return:
+        :return: The index of the selected individual
         """
         if len(fitness_ordered_pop) <= self.k:
-            return fitness_ordered_pop[0]
+            return 0
+        if isinstance(fitness_ordered_pop[0], Individual):
+            fitness_ordered_pop = list(Individual.get_genes(fitness_ordered_pop))
 
         idxs_subset = sorted([np.random.randint(len(fitness_ordered_pop)) for _ in range(self.k)])
         # generate k random uniform numbers on [0,1] and check which is less than p, hence
         # return the index of the first.
         idx = np.argmax(np.random.uniform(0, 1, self.k) < self.p)   # idx relative to idxs_subset
-        idx = idxs_subset[idx]
-        return fitness_ordered_pop[idx]
+        return idxs_subset[idx]
+
+
+class Individual:
+    """
+    A convenient representation of an individual for the computation. This representation contains the genetic material
+    and the score associated with this individual.
+    """
+
+    def __init__(self, gene, score=None):
+        self.gene = gene
+        self.score = score
+
+    @staticmethod
+    def get_scores(population):
+        return map(lambda p: p.score, population)
+
+    @staticmethod
+    def get_genes(population):
+        return map(lambda p: p.gene, population)
+
+    def copy(self):
+        return Individual(gene=dict(self.gene), score=self.score)
 
 
 class MutationOnlyEvoStrategy(GeneticBase):
     """
-    1+lambda evolutionary strategy
+    mu+lambda evolutionary strategy
     """
 
-    def __init__(self, graph: hgg.Graph, fitness, *, opt_mode='max', lambda_=4, generations=10**4, target_score=None):
+    def __init__(self, graph: hgg.Graph, fitness, *,
+                 opt_mode='max', mutation_prob=0.1,
+                 population_size=1, lambda_=4, elitism=1, generations=10**4, target_score=None,
+                 selector=TournamentSelection()):
         # TODO callback
         # TODO validate params
         if opt_mode not in ['min', 'max']:
@@ -158,77 +184,112 @@ class MutationOnlyEvoStrategy(GeneticBase):
 
         self.fitness = fitness
         self.opt_mode = opt_mode
+        self.mutation_prob = mutation_prob
+        self.population_size = population_size
         self.lambda_ = lambda_
+        self.elitism = elitism
         self.generations = generations
+        self.selector = selector
         super().__init__(graph=graph)
-        self.parent = None
-        self.parent_score = None
+
+        self.population = None
+        self._best = None
         self.target_score = None if target_score is None else float(target_score)
         self.history = None
 
     def reset(self):
-        self.parent = None
-        self.parent_score = None
+        self.population = None
+        self._best = None
+
         self.history = History()
 
     @property
-    def best(self):
+    def best(self):     # TODO return Individual
         """
-        The best individual
+        Return the genetic material of the best individual
         :return:
         """
-        p = self.parent
-        return None if p is None else dict(p)
+        p = self._best
+        return None if p is None else dict(p.gene)
+
+    def _apply_fitness(self, population):
+        fitness = self.fitness
+        for p in population:
+            p.score = fitness(p.gene)
+
+    def _create_parent_offspring(self, parent):
+        p = self.mutation_prob
+        l = self.lambda_
+        return [Individual(self.mutations(parent.gene, prob=p)) for _ in range(l)]
 
     def __call__(self):
         self.reset()
 
-        parent = self.parent
-        parent_score = self.parent_score
+        population = self.population
         target_score = self.target_score
-        fitness = self.fitness
-        opt_mode = self.opt_mode
+        selector = self.selector
 
         def score_cmp_min(a, b):
             return a <= b
 
+        def sort_min(a):
+            a = list(a)
+            return np.argsort(a)
+
         def score_cmp_max(a, b):
             return a >= b
 
-        score_cmp = score_cmp_min if opt_mode == 'min' else score_cmp_max
+        def sort_max(a):
+            a = list(a)
+            return np.argsort(a)[::-1]
 
-        if parent is None:
-            parent = self.create_population()
-        if parent_score is None:
-            parent_score = fitness(parent)
-            if parent_score is None:
-                raise ValueError()
+        def apply_perm(lst, idxs):
+            return [lst[i] for i in idxs]
+
+        score_cmp = score_cmp_min if self.opt_mode == 'min' else score_cmp_max
+        sort_op = sort_min if self.opt_mode == 'min' else sort_max
+
+        def update_best(candidate: Individual):
+            if (self._best is None) or score_cmp(candidate.score, self._best.score):
+                self._best = candidate.copy()
+                return True
+            return False
+
+        # create initial population
+        if population is None:
+            population = [Individual(p) for p in self.create_population(size=self.population_size)]
+            self._apply_fitness(population)
+            population = apply_perm(population, sort_op(Individual.get_scores(population)))
 
         for c in range(self.generations):
-            hit = 0
-            offspring = [self.mutations(parent, prob=np.random.uniform()) for _ in range(self.lambda_)]
-            for child in offspring:
-                score = fitness(child)
-                if score is None:
-                    break
-                # TODO define score_thr, when achieved, stop
-                if score_cmp(score, parent_score):
-                    parent = child
-                    parent_score = score
-                    hit = 1
+            # create new offspring
+            offspring = itertools.chain(*[self._create_parent_offspring(parent) for parent in population])
+            offspring = list(offspring)
+            self._apply_fitness(offspring)
+            offspring.extend(population[:self.elitism])     # preserve the best parents
+            offspring = apply_perm(offspring, sort_op(Individual.get_scores(offspring)))
 
-            self.parent = parent
-            self.parent_score = parent_score
+            hit = update_best(offspring[0])     # update the best ever
+
+            # apply selection strategy and create a new population
+            selection = set()
+            while len(selection) != self.population_size:
+                selection.add(selector.select(offspring))
+                # TODO in certain cases we may have an infinite loop...
+            population = apply_perm(offspring, sorted(selection))
+            self.population = population
+            # TODO check order
+            del offspring
+
             if hit:
                 # TODO move to a specific callback
-                self.history.generations.append({'idx': c, 'best_score': parent_score}) # TODO include datetime
-                if target_score is not None:
-                    if score_cmp(parent_score, target_score):
-                        print("*** target score achieved ***")
-                        break
+                self.history.generations.append({'idx': c, 'best_score': self._best.score})     # TODO include datetime
+                if (target_score is not None) and score_cmp(self._best.score, target_score):
+                    print("*** target score achieved ***")
+                    break
 
             if c % 100 == 0:
                 # TODO move to a specific callback
                 print("**** **** ****")
-                print("best: "+str(self.parent))
-                print("best_score: "+str(self.parent_score)+", generation: "+str(c))
+                mean_score = np.mean(list(Individual.get_scores(population)))
+                print("generation: "+str(c) + ", best_score: " + str(self._best.score) + ", mean_score: " + str(mean_score))
